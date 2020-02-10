131b7ed4143e68ff5950a8cd1df5e6fd8efb4723
Alyssia <alyssia0207@gmail.com>
2020-02-10 11:44:29 +0100

    Story 1 done

--- src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -4,12 +4,21 @@ import fr.rphstudio.chess.interf.EmptyCellException;
 import fr.rphstudio.chess.interf.IChess;
 import fr.rphstudio.chess.interf.OutOfBoardException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChessModel implements IChess {
 
+    private static ChessModel instance;
+
+    private ChessModel(){
+    }
+
     public static IChess getInstance() {
-        return null;
+        if(ChessModel.instance==null){
+            ChessModel.instance = new ChessModel();
+        }
+        return ChessModel.instance;
     }
 
     @Override
@@ -19,12 +28,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessType.TYP_PAWN;
     }
 
     @Override
     public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessColor.CLR_WHITE;
     }
 
     @Override
@@ -34,7 +43,7 @@ public class ChessModel implements IChess {
 
     @Override
     public List<ChessPosition> getPieceMoves(ChessPosition p) {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
@@ -44,12 +53,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessKingState getKingState(ChessColor color) {
-        return null;
+        return ChessKingState.KING_SAFE;
     }
 
     @Override
-    public List<ChessType> getRemovedPieces(ChessColor color) {
-        return null;
+    public List<ChessType> getRemovedPieces(ChessColor color){
+        return new ArrayList<>();
     }
 
     @Override
