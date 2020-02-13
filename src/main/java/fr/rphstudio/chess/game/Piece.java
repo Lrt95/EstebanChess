c24c2cc7efc17c1cab21965bd94c823e537bd7c3
Laurent <laurent.sem@edu.itescia.fr>
2020-02-13 09:42:43 +0100

    JavaDoc Piece done

--- src/main/java/fr/rphstudio/chess/game/Piece.java
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -10,6 +10,12 @@ public class Piece {
     private IMove move;
     private boolean firstMove;
 
+    /**
+     * Property of a piece
+     * @param pieceColor the color of the piece
+     * @param pieceType the type of the piece
+     */
+
     public Piece(IChess.ChessColor pieceColor, IChess.ChessType pieceType) {
         this.pieceColor = pieceColor;
         this.pieceType = pieceType;
@@ -30,29 +36,58 @@ public class Piece {
         }
     }
 
+    /**
+     * Get the color of the piece
+     * @return the piece color
+     */
+
     public IChess.ChessColor getPieceColor() {
         return this.pieceColor;
     }
 
+    /**
+     * Get the type of the piece
+     * @return the type piece
+     */
     public IChess.ChessType getPieceType() {
         return this.pieceType;
     }
 
+    /**
+     * Set the color of the piece
+     * @param pieceColor the piece color
+     */
     public void setPieceColor(IChess.ChessColor pieceColor) {
         this.pieceColor = pieceColor;
     }
 
+    /**
+     * Set the type of the piece
+     * @param pieceType the type piece
+     */
     public void setPieceType(IChess.ChessType pieceType) {
         this.pieceType = pieceType;
     }
 
+    /**
+     * Get the piece's first move
+     * @return the first move of the piece
+     */
     public boolean isFirstMove() {
         return firstMove;
     }
 
+    /**
+     * If true, pieces advances of two squares.
+     */
     public void setFirstMoveFalse() {
         this.firstMove = false;
     }
+
+    /**
+     * Get the move of a piece
+     * @return the move of the piece
+     */
 
     public IMove getMove() {
         return move;
