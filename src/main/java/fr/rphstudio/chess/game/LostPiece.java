86ab3352bd45d973f3a142f362aaff75d472528f
Laurent <laurent.sem@edu.itescia.fr>
2020-02-13 09:46:12 +0100

    JavaDoc LostPiece done

--- src/main/java/fr/rphstudio/chess/game/LostPiece.java
+++ src/main/java/fr/rphstudio/chess/game/LostPiece.java
@@ -10,10 +10,20 @@ public class LostPiece {
     private List<IChess.ChessType> lostPiecesW;
     private List<IChess.ChessType> lostPiecesB;
 
+    /**
+     * List of all black and white pieces lost
+     */
+
     public LostPiece() {
         lostPiecesB = new ArrayList<>();
         lostPiecesW = new ArrayList<>();
     }
+
+    /**
+     * Add the pieces lost during game
+     * @param chessType the type of the pieces
+     * @param chessColor the color of the pieces
+     */
 
     public void addType(IChess.ChessType chessType, IChess.ChessColor chessColor) {
         if (chessColor == IChess.ChessColor.CLR_WHITE) {
@@ -24,6 +34,11 @@ public class LostPiece {
         }
     }
 
+    /**
+     * Get the lost pieces lists
+     * @param chessColor the color of the pieces
+     * @return the list of lost pieces depending on the color
+     */
     public List<IChess.ChessType> getList(IChess.ChessColor chessColor) {
         if (chessColor == IChess.ChessColor.CLR_WHITE) {
            return lostPiecesW;
